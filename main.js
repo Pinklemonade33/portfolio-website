ce887d4fd4d652aecb1767ab9f6a63d943a88126
Braiden <69333220+Pinklemonade33@users.noreply.github.com>
2023-03-21 08:51:52 -0500

    commit

--- main.js
+++ main.js
@@ -1,5 +1,138 @@
-// Imports
-import * as functions from './functions.js';
+//Functions
+
+// Gets class associated with skill elements
+// skill class example: skills-js
+const getSkillClass = (element) => {
+    return Array.from(element.classList).find(
+        cls => cls.slice(0, 5) === 'skill'
+    )
+}
+
+// Get elements of specified class, reduce to specified tags
+const getClassTags = (cls, tags) => {
+    return Array.from(document.getElementsByClassName(cls)).reduce(
+        (acc, element) => {
+            if (tags.some(tag => tag === element.tagName.toLocaleLowerCase())) {
+                acc.push(element);
+            }
+            return acc;
+        }, []
+    );
+}
+
+// Gets skill elements containing text class
+// Element class example: skills-js text
+const getSkillText = (cls) => {
+    return skillText.reduce((acc, element) => {
+        if (Array.from(element.classList).some(t => t === cls)) {
+            acc.push(element)
+        }
+        return acc
+    }, [])
+}
+
+// Click event for skill buttons
+const clickButton = (event) => {
+    if (previousSkill != null) {
+        const hideClass = getSkillClass(previousSkill)
+        const hideElements = getSkillText(hideClass)
+        hideElements.forEach(element => element.remove());
+        previousSkill.style.background = '#3f4045ff'
+    }
+
+    const showClass = getSkillClass(event.target)
+    const showElements = getSkillText(showClass)
+    showElements.forEach(element => {
+        if (element.tagName.toLocaleLowerCase() === 'div') {
+            expContainer.appendChild(element)
+        } else {
+            headingContainer.appendChild(element)
+        }
+    });
+
+    const container = divSkillClasses[showClass];
+    container.style.background = 'hsla(309, 8%, 12%, 1)';
+    activeButton = container;
+    previousSkill = divSkillClasses[showClass];
+}
+
+
+
+// Change opacity events for skill switch
+const changeOpacityLow = (event) => {
+    event.target.style.opacity = '0.1';
+}
+const changeOpacityHigh = (event) => {
+    event.target.style.opacity = '1';
+}
+
+// Toggles hover for skill switch
+const switchHover = (element) => {
+    if (element === hardSwitch) {
+        softSwitch.addEventListener('mouseout', changeOpacityLow);
+        hardSwitch.removeEventListener('mouseout', changeOpacityLow);
+    } else {
+        hardSwitch.addEventListener('mouseout', changeOpacityLow);
+        softSwitch.removeEventListener('mouseout', changeOpacityLow);
+    }
+}
+
+// Switches the skill switch bar
+const switchSkills = (event) => {
+    if (event.target === softSwitch) {
+        hardSwitch.style.opacity = '0.1';
+        softSwitch.style.opacity = '1';
+        softSkillsContainer.style.opacity = '1';
+        hardSkillsContainer.style.opacity = '0'
+        softSkillsContainer.style.zIndex = '1'
+        hardSkillsContainer.style.zIndex = '0'
+        switchHover(event.target)
+    } else {
+        hardSwitch.style.opacity = '1';
+        softSwitch.style.opacity = '0.1';
+        softSkillsContainer.style.opacity = '0';
+        hardSkillsContainer.style.opacity = '1'
+        softSkillsContainer.style.zIndex = '0'
+        hardSkillsContainer.style.zIndex = '1'
+        switchHover(event.target)
+    }
+}
+
+// Hover events for skill buttons
+const hoverButtonIn = (event) => {
+    const skillClass = getSkillClass(event.target)
+    const container = divSkillClasses[skillClass]
+    
+    if (container != activeButton) {
+        container.style.background = '#30292fff'
+    } else {
+        container.style.background = 'hsla(309, 8%, 11%, 1)';
+    }
+}
+const hoverButtonOut = (event) => {
+    const skillClass = getSkillClass(event.target)
+    const container = divSkillClasses[skillClass]
+
+    if (container != activeButton) {
+        container.style.background = '#3f4045ff'
+    } else {
+        container.style.background = 'hsla(309, 8%, 11%, 1)';
+    }
+}
+
+const setHeight = () => {
+    softSkillsInner.style.height = getComputedStyle(softSkillsContainer).height
+    hardSkillsInner.style.height = getComputedStyle(hardSkillsContainer).height
+}
+
+const addText = (paragraph) => {
+    const p = document.createElement('p');
+    p.innerHTML = paragraph.text;
+    p.id = paragraph.id;
+    p.style.margin = '0';
+    p.style.fontSize = '24px';
+    textContainer.appendChild(p)
+}
 
 // Globals
 const textContainer = document.getElementById('text-container'); 
@@ -37,29 +170,21 @@ const divSkillClasses = allSkills.reduce(
     }, {}
 );
 
-const Paragraphs = [
-    {text: `I learned I was passionate about coding when my Excel project turned into a 
-    VBA project that kept growing beyond my original ideas that I had set for it.
-    
-    /n/n I had no prior training at all when I tried to fix a script that was supposed
-    to take all the cells of a certain color and add them together. None of 
-    the tutorials I found helped me much, I guess they gave me a faded dirt path I could 
-    follow but that was about it. I was jumping into to something that was much bigger than 
-    I could have imagined at the time and there were so many questions that I needed answered
-    that I didn't know needed asking. Obviously I wasn't able to fix the script as I 
-    thought I could so easily do, and without realizing it, I stepped onto a road that will never end.`, id: 'how'},
-    {text: ``, id: 'where'}
-]
+/* Fades in intro text
+ {
+    $(function() {
+        $("#intro span").each(function(i) {
+            $(this).hide();
+        });
+    });
+    $(function() {
+        $("#intro span").each(function(i) {
+            $(this).delay(i * 500).fadeIn(1500);
+        });
+    });
+};
+*/
 
-
-const addText = (paragraph) => {
-    const p = document.createElement('p');
-    p.innerHTML = paragraph.text;
-    p.id = paragraph.id;
-    p.style.margin = '0';
-    p.style.fontSize = '24px';
-    textContainer.appendChild(p)
-}
 
 hardSwitch.addEventListener('click', switchSkills)
 softSwitch.addEventListener('click', switchSkills)
